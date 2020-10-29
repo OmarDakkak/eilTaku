221c0f5b1672ae594e5791de9ab8582383da6693
Omar Dakkak <omardakkak98@gmail.com>
2020-10-29 22:20:14 +0100

    Update routes/api/users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,6 +51,31 @@ router.post('/register', (req, res) => {
 // @route   GET api/users/login 
 // @desc    Login user / Returning JWT Token
 // @access  Public
-router.post()
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find the user by email
+    User
+        .findOne({email})
+        .then(user => {
+            // Check for user
+            if(!user){
+                return res.status(404).json({email: 'User not found!'});
+            }
+
+            // Check the password
+            bcrypt
+                .compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        res.json({msg: 'Success!'});
+                    }else{
+                        return res.status(400).json({password: 'Incorrect Password!'});
+                    }
+                });
+
+        });
+});
 
 module.exports = router;
