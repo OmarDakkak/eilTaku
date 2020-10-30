7013a7ddbbf68674f1821fbbfabbdfceec619257
Omar Dakkak <omardakkak98@gmail.com>
2020-10-30 22:47:12 +0100

    Added JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoURI : 'mongodb+srv://OmarDakkak:taza1998@eiltaku.tgz4p.mongodb.net/test?retryWrites=true&w=majority',
-}
+    secretOrKey: 'hadqdyqw8213ojeUYE8Ueyda91xw10'
+};
